2ab70bbafa9986792f6f176545746c29d52a93ec
sahilkhoja23 <sahilkhoja23@gmail.com>
2015-03-24 23:42:52 -0500

    Implementing Alamofire

--- PeePal/PeePal/ViewController.swift
+++ PeePal/PeePal/ViewController.swift
@@ -5,6 +5,14 @@
 //  Created by Sahil Khoja on 3/24/15.
 //  Copyright (c) 2015 Sahil Khoja. All rights reserved.
 //
+// using json within the table
+//            if (json != nil){
+//
+//                var jsonObject = JSON(json!)
+//                if let data = jsonObject["data"].arrayValue as [JSON]? {
+//                    self.results = data
+//                    self.tableView.reloadData()
+
 
 import UIKit
 
@@ -19,7 +27,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func loadDogs(){
+        let urlss = "http://api.sportsdatallc.org/nba-t3/games/d7858b35-8e5c-48b5-86d3-67c41217bd3a/pbp.json?api_key=krqfh348vvrt2x8gtwrznxsb"
+        Alamofire.request(.GET, urlss).responseJSON{(request, response, json, error) in
+        //Alamofire.request(.GET, urlss).responseJSON{(request, response, json, error) in
+        
+        println(json)
+    
 
 }
 
